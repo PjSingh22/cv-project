a94efe430157b1d88da05d28891bf8b84c10822c
Prabhjot Singh <pjsingh1359@gmail.com>
2022-06-06 12:01:17 -0700

    add state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Resume from './components/Resume';
+
+class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      resume: {
+        name: '',
+        email: '',
+        phone: '',
+        address: '',
+        city: '',
+        state: '',
+        zip: '',
+        summary: '',
+        skills: [],
+        education: [],
+        experience: [],
+        projects: [],
+        languages: [],
+        interests: [],
+        references: [],
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <h1>Resume Generator</h1>
+        <form>
+          <label>
+            Name:
+            <input type='text' name='name' required />
+          </label>
+          <label>
+            Email:
+            <input type='email' name='email' required />
+          </label>
+          <label>
+            Phone:
+            <input type='text' name='phone' required />
+          </label>
+          <label>
+            Address:
+            <input type='text' name='address' required />
+          </label>
+          <label>
+            City:
+            <input type='text' name='city' required />
+          </label>
+          <button type='submit'>Submit</button>
+        </form>
+        <Resume />
+      </div>
+    );
+  };
+};
 
 export default App;
